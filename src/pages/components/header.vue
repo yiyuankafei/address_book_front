4b81ea730e9e22a608d90f74fd701af9788bd3d3
yiyuankafei <124633367@qq.com>
2019-11-11 23:57:39 +0800

    添加按钮 iconfont

--- src/pages/components/header.vue
+++ src/pages/components/header.vue
@@ -1,6 +1,7 @@
 <template>
 	<div class="header">
 		通讯录
+		<div class="iconfont add-icon">&#xe81b;</div>
 	</div>
 </template>
 
@@ -15,4 +16,13 @@ export default {
 		position:relative
 		overflow:hidden
 		height:0.86rem
+		line-height:.86rem
+		background:#eee
+		font-size:.35rem
+		font-weight:700
+		text-align:center
+		.add-icon
+			position:fixed
+			right:.45rem
+			top:0
 </style>
